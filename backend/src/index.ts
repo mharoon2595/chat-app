0971df4b0b7ae1a96e61c1c2539cd17100b86a80
Haroon <131857305+mharoon2595@users.noreply.github.com>
2024-07-24 23:49:32 +0530

    final

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,7 @@ import authRoutes from "./routes/auth.route.js"
 import messageRoutes from "./routes/message.route.js"
 import { Request,Response, NextFunction } from "express";
 import cors from "cors"
+import path from "path";
 import { app, server } from "./socket/socket.js";
 import cookieParser from "cookie-parser";
 
@@ -33,6 +34,13 @@ app.use((error:any, req:Request, res:Response, next:NextFunction) => {
     .status(error.code || 500)
     .json({ message: error.message || "An unknown error occured" });
 });
+
+if (process.env.NODE_ENV !== "development") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+	app.get("*", (req, res) => {
+		res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+	});
+}
 
 server.listen(3000, () => {
 	console.log("Server is running on port " + 3000);

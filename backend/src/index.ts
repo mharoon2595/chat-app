c6e9a17ee67578b54d2816f5690b104f170d85ce
Haroon <131857305+mharoon2595@users.noreply.github.com>
2024-07-25 06:58:49 +0530

    .

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -21,20 +21,20 @@ app.use(express.json())
 app.use("/api/auth",authRoutes)
 app.use("/api/messages", messageRoutes)
 
-app.use((req, res, next) => {
-  const error = new Error("Unsupported route");
-  throw error;
-});
+// app.use((req, res, next) => {
+//   const error = new Error("Unsupported route");
+//   throw error;
+// });
 
-app.use((error:any, req:Request, res:Response, next:NextFunction) => {
+// app.use((error:any, req:Request, res:Response, next:NextFunction) => {
   
-  if (res.headersSent) {
-    return next(error);
-  }
-  res
-    .status(error.code || 500)
-    .json({ message: error.message || "An unknown error occured" });
-});
+//   if (res.headersSent) {
+//     return next(error);
+//   }
+//   res
+//     .status(error.code || 500)
+//     .json({ message: error.message || "An unknown error occured" });
+// });
 const __dirname = path.resolve();
 if (process.env.NODE_ENV !== "development") {
 	app.use(express.static(path.join(__dirname, "/frontend/dist")));
